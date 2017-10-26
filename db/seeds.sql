e444089bcd1fad210188b63475306edb93285fc4
Skylar Powers <skylarjpowers@gmail.com>
2017-10-26 08:43:28 -0700

    final

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Style Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheeseburger', false);
